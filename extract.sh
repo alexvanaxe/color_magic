36f65373ea032a3ace285940adf97430aec73b6a
Alexvanaxe <alexvanaxe@gmail.com>
2020-08-15 21:12:02 -0300

    Text color tests

--- extract.sh
+++ extract.sh
@@ -3,7 +3,11 @@
 . color_sort.sh
 mkdir img
 rm -rf ./img/*
-wallpaper=$(cat ~/wallpaper.txt)
+
+wallpaper=$1
+if [ -z "$wallpaper" ]; then
+    wallpaper=$(cat ~/wallpaper.txt)
+fi
 
 
 colors=($(convert "${wallpaper}" -format %c -depth 8 -colors 15 histogram:info:- | sort -n | grep -o "#......"))
@@ -16,10 +20,15 @@ colors_lum=($(order_list $colors))
 
 count=0
 echo -e ${colors_lum[@]} | sort -n
-echo -e ${colors_lum[@]}  | sort -n | awk '{print $2}'
+#echo -e ${colors_lum[@]}  | sort -n | awk '{print $2}'
 
-for corsort in $(echo -e ${colors_lum[@]}  | sort -n | awk '{print $2}'); do
-    ./generate_img.sh "${corsort}" "$(printf %02d $count)_${corsort}"
-    count=$(( count + 1 ))
-done
-#
+
+#for corsort in $(echo -e ${colors_lum[@]}  | sort -n | awk '{print $2}'); do
+    #./generate_img.sh "${corsort}" "$(printf %02d $count)_${corsort}"
+    #count=$(( count + 1 ))
+#done
+##
+
+echo "COLOR:"
+echo $(black_or_white $colors 11)
+
